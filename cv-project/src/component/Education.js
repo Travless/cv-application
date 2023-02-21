6f2dd6ddd1b92eb25520588f27850b13794a9d7e
Travless <travis.white0101@gmail.com>
2023-02-21 11:36:55 -0500

    Create three components based on the main three sections of the CV: GeneralInfo, Education, Experience

---
+++ cv-project/src/component/Education.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Education extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Education;
