1ce2cf80caf7568a0d7fbdd77c44190853e78a21
Travless <travis.white0101@gmail.com>
2023-02-21 14:54:39 -0500

    Create change and submit handlers for Experience component. Tasks should be able to house multiple job responsibilities for each job listed

--- cv-project/src/component/Experience.js
+++ cv-project/src/component/Experience.js
@@ -2,14 +2,44 @@ import React from "react";
 
 class Experience extends React.Component {
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+            experience: {
+                company: '',
+                position: '',
+                tasks: [],
+                start: '',
+                end: '',
+            }
+        }
     }
 
     render(){
         return(
-            <div>
-
-            </div>
+            <form className="form" >
+                <div className="companyNameCont" >
+                    <label htmlFor="companyNameInput" >Company Name: </label>
+                    <input type="text" id="companyNameInput" value={} onChange={} />
+                </div>
+                <div className="positionTitleCont" >
+                    <label htmlFor="positionTitleInput" >Position Title: </label>
+                    <input type="text" id="positionTitleInput" value={} onChange={} />
+                </div>
+                <div className="tasksCont" >
+                    <label htmlFor="tasksInput" >Job Responsibilities: </label>
+                    <input type="text" id="tasksInput" value={} onChange={} />
+                </div>
+                <div className="startCont" >
+                    <label htmlFor="startInput" >Start Date: </label>
+                    <input type="text" id="startInput" value={} onChange={} />
+                </div>
+                <div className="endCont" >
+                    <label htmlFor="endInput" >End Date: </label>
+                    <input type="text" id="endInput" value={} onChange={} />
+                </div>
+                <button className="submit" type="submit" >Submit</button>
+            </form>
         )
     }
 }
