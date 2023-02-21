fbc0acadd7e4725a04c864ba2847dc1ded557394
Travless <travis.white0101@gmail.com>
2023-02-21 14:16:40 -0500

    Create Education component that takes in school name, degree, and graduation year. Import Education component to App component

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import GeneralInfo from "./component/GeneralInfo";
+import Education from "./component/Education";
 
 function App() {
   return (
     <div>
       <GeneralInfo />
+      <Education />
     </div>
   );
 }
--- cv-project/src/component/Education.js
+++ cv-project/src/component/Education.js
@@ -2,14 +2,63 @@ import React from "react";
 
 class Education extends React.Component {
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+            education: {
+                school: '',
+                degree: '',
+                grad: '',
+            }
+        };
+
+        this.handleSchoolChange = this.handleSchoolChange.bind(this);
+        this.handleDegreeChange = this.handleSchoolChange.bind(this);
+        this.handleGradChange = this.handleGradChange.bind(this);
     }
+
+    handleSchoolChange = (e) => {
+        this.setState({
+            education: {
+                school: e.target.value
+            }
+        });
+    };
+
+    handleDegreeChange = (e) => {
+        this.setState({
+            education: {
+                degree: e.target.value
+            }
+        });
+    };
+
+    handleGradChange = (e) => {
+        this.setState({
+            education: {
+                grad: e.target.value
+            }
+        })
+    }
+
 
     render(){
         return(
-            <div>
-
-            </div>
+            <form className="form">
+                <div className="schoolCont">
+                    <label htmlFor="schoolInput" >School Name: </label>
+                    <input type="text" id="schoolInput" value={this.state.education.school} onChange={this.handleSchoolChange} />
+                </div>
+                <div className="degreeCont" >
+                    <label htmlFor="degreeInput" >Degree: </label>
+                    <input type="text" id="degreeInput" value={this.state.education.degree} onChange={this.handleDegreeChange} />
+                </div>
+                <div className="gradDateCont" >
+                    <label htmlFor="gradDateInput" >Graduation Year: </label>
+                    <input type="date" id="gradDateInput" value={this.state.education.grad} onChange={this.handleGradChange} />
+                </div>
+                <button className="submit" type="submit">Submit</button>
+            </form>
         )
     }
 }
